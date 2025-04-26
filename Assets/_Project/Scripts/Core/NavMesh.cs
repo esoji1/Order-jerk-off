ce940f6f4bdee80a5e5c90ce52776da0dcd11f41
Esoji <aim.wtf1234@gmail.com>
2025-04-26 18:09:34 +0300

    Changed the technical part of the location and added AI

---
+++ Assets/_Project/Scripts/Core/NavMesh.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMesh : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+
+    private NavMeshAgent _agent;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.updateRotation = false;
+        _agent.updateUpAxis = false;
+    }
+
+    private void Update()
+    {
+        _agent.SetDestination(_target.position);
+    }
+}
